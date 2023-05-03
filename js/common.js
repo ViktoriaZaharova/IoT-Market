40df2de4707d1825a42b878676c6a3640efa3d92
Виктория Захарова <zaharova181114@gmail.com>
2023-05-03 17:52:52 +0300

    add catalog

--- js/common.js
+++ js/common.js
@@ -23,3 +23,34 @@ $('.btn-burger').click(function () {
 $('.nav-menu__close').click(function () {
 	$('.nav-menu').fadeOut();
 });
+
+// price slider
+$(document).ready(function () {
+	$('.slider-range').slider({
+		range: true,
+		min: 2000,
+		max: 65000,
+		values: [15000, 65000],
+		classes: {
+			"ui-slider-handle": "ui-corner-all"
+		},
+		slide: function (event, ui) {
+			//Поле минимального значения
+			$(".dec1").val(ui.values[0]);
+			//Поле максимального значения
+			$(".dec2").val(ui.values[1]);
+		}
+	});
+
+	$(".dec1").val($(".slider-range").slider("values", 0));
+	$(".dec2").val($(".slider-range").slider("values", 1));
+
+});
+
+$('.btn-filter').click(function () {
+	$('.sidebar').fadeToggle();
+});
+
+$('.sidebar-close').click(function () {
+	$('.sidebar').fadeOut();
+});
