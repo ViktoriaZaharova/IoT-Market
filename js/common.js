f2b317e1dfb0f1b4749219b0f5258fc4004d6e25
Виктория Захарова <zaharova181114@gmail.com>
2023-05-04 17:50:16 +0300

    all pages

--- js/common.js
+++ js/common.js
@@ -54,3 +54,79 @@ $('.btn-filter').click(function () {
 $('.sidebar-close').click(function () {
 	$('.sidebar').fadeOut();
 });
+
+$('.down').on("click", function () {
+	var $input = $(this).parent().find('input');
+	var count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+	$input.change();
+	return false;
+});
+$('.up').on("click", function () {
+	var $input = $(this).parent().find('input');
+	$input.val(parseInt($input.val()) + 1);
+	$input.change();
+	return false;
+});
+
+
+$('.product-gallery-max').slick({
+	slidesToShow: 1,
+	fade: true,
+	arrows: false,
+	swipe: false,
+});
+
+$('.product-gallery-preview').slick({
+	slidesToShow: 4,
+	arrows: false,
+	vertical: true,
+	asNavFor: '.product-gallery-max',
+	focusOnSelect: true,
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 3,
+				vertical: true,
+			}
+		}
+	]
+});
+
+function setProgress(index) {
+	var calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
+
+	$progressBar
+		.css('width', calc + '%')
+		.attr('aria-valuenow', calc);
+}
+
+var $slider = $('.product-gallery-max');
+var $progressBar = $('.progress-bg');
+
+$slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+	setProgress(nextSlide);
+
+});
+setProgress(0);
+
+$(".form-checkout input, .form-checkout textarea").focus(function () {
+	$(this).parent().addClass("focus");
+}).blur(function () {
+	if ($(this).val() === '') {
+		$(this).parent().removeClass("focus");
+	}
+});
+
+$('.form-checkout input, .form-checkout textarea').each(function () {
+	var $this = $(this),
+		val = $this.val();
+
+	if (val.length >= 1) {
+		$(this).parent().addClass("focus");
+	} else {
+		$(this).parent().removeClass("focus");
+	}
+});
