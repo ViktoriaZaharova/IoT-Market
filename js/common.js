19271033498011451aaa9ba7c6610c6b64e1be17
Виктория Захарова <zaharova181114@gmail.com>
2023-05-06 12:10:16 +0300

    edit

--- js/common.js
+++ js/common.js
@@ -24,6 +24,11 @@ $('.nav-menu__close').click(function () {
 	$('.nav-menu').fadeOut();
 });
 
+$('.btn-search').on('click', function (e) {
+	e.preventDefault();
+	$(this).siblings('.form-search').fadeToggle();
+});
+
 // price slider
 $(document).ready(function () {
 	$('.slider-range').slider({
@@ -45,7 +50,43 @@ $(document).ready(function () {
 	$(".dec1").val($(".slider-range").slider("values", 0));
 	$(".dec2").val($(".slider-range").slider("values", 1));
 
+
+	$(".range_val input").change(function () {
+		var input_left = $(".dec1").val().replace(/[^0-9]/g, ''),
+			opt_left = $(".slider-range").slider("option", "min"),
+			where_right = $(".slider-range").slider("values", 1),
+			input_right = $(".dec2").val().replace(/[^0-9]/g, ''),
+			opt_right = $(".slider-range").slider("option", "max"),
+			where_left = $(".slider-range").slider("values", 0);
+		if (input_left > where_right) {
+			input_left = where_right;
+		}
+		if (input_left < opt_left) {
+			input_left = opt_left;
+		}
+		if (input_left == "") {
+			input_left = 0;
+		}
+		if (input_right < where_left) {
+			input_right = where_left;
+		}
+		if (input_right > opt_right) {
+			input_right = opt_right;
+		}
+		if (input_right == "") {
+			input_right = 0;
+		}
+		$(".dec1").val(input_left);
+		$(".dec2").val(input_right);
+		if (input_left != where_left) {
+			$(".slider-range").slider("values", 0, input_left);
+		}
+		if (input_right != where_right) {
+			$(".slider-range").slider("values", 1, input_right);
+		}
+	});
 });
+
 
 $('.btn-filter').click(function () {
 	$('.sidebar').fadeToggle();
@@ -130,3 +171,26 @@ $('.form-checkout input, .form-checkout textarea').each(function () {
 		$(this).parent().removeClass("focus");
 	}
 });
+
+// hidden list > 5
+$('.accordion-item .accordion-body .list-checkbox ').each(function () {
+	if ($(this).find('li').length > 4) {
+		$(this).find('li').slice(4).hide();
+		$(this).parents('.accordion-body').append('<a href="#" class="load-more-wrap color-red">Показать еще</a>');
+	}
+
+});
+// hidden list > 5
+
+// show list all
+$('.load-more-wrap').on('click', function (e) {
+	e.preventDefault();
+	$(this).prev('.accordion-item .accordion-body .list-checkbox').find('li').slideDown();
+
+	var onBlock = $(this).prev('.acordion-item .accordion-body .list-checkbox ').find('li:hidden').length;
+	if (onBlock <= 0) {
+		$(this).hide();
+	}
+});
+
+	// show list all
